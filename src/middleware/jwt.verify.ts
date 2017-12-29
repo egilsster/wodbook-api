41f35e072e4c6b1e6bee4fe4198c4ee67f42bd6e
Egill Sveinbjörnsson <egillsveinbjorns@gmail.com>
2017-12-29 13:08:16 +0000

    Unit tests for JwtVerify middleware

--- src/middleware/jwt.verify.ts
+++ src/middleware/jwt.verify.ts
@@ -5,13 +5,12 @@ export default (cert) => {
 	return (req, res, next) => {
 		const decodedCert = Buffer.from(cert, 'base64').toString('UTF-8');
 
-		jwt.verify(req.token, decodedCert, function (err, user) {
-			if (err) {
-				res.sendStatus(HttpStatus.UNAUTHORIZED);
-			} else {
-				req.user = user;
-				next();
-			}
-		});
+		try {
+			const user = jwt.verify(req.token, decodedCert);
+			req.user = user;
+			next();
+		} catch (err) {
+			res.sendStatus(HttpStatus.UNAUTHORIZED);
+		}
 	};
 };
