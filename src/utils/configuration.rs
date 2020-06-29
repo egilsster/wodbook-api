d6528eae093c8a63537c55f4979add8aeae7470e
Egill Sveinbjörnsson <egillsveinbjorns@gmail.com>
2020-06-29 17:07:27 +0200

    feat: allow unset config variables
    defaulting the host and port config

--- src/utils/configuration.rs
+++ src/utils/configuration.rs
@@ -3,6 +3,14 @@ use mongodb::Client;
 use serde::Deserialize;
 use slog::{o, Drain, Logger};
 
+fn default_server_host() -> String {
+    "127.0.0.1".to_string()
+}
+
+fn default_server_port() -> i32 {
+    43210
+}
+
 #[derive(Clone)]
 pub struct AppState {
     pub mongo_client: Client,
@@ -22,7 +30,9 @@ pub struct AuthConfig {
 
 #[derive(Deserialize)]
 pub struct Config {
+    #[serde(default = "default_server_host")]
     pub host: String,
+    #[serde(default = "default_server_port")]
     pub port: i32,
 
     pub auth: AuthConfig,
@@ -32,7 +42,11 @@ pub struct Config {
 impl Config {
     pub fn from_env() -> Result<Self, ConfigError> {
         let mut cfg = config::Config::new();
-        cfg.merge(config::Environment::new().separator("__"))?;
+        cfg.merge(
+            config::Environment::new()
+                .separator("__")
+                .ignore_empty(true),
+        )?;
         cfg.try_into()
     }
 
