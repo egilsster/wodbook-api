22d586cb2ba382c0cbda4151702550274530eb47
Egill Sveinbjörnsson <egilsster@users.noreply.github.com>
2020-06-29 14:22:34 +0200

    refactor: simplify mongo config options (#176)

--- src/db/connection.rs
+++ src/db/connection.rs
@@ -8,14 +8,13 @@ pub struct Connection;
 impl Connection {
     pub async fn get_client(&self, logger: Logger) -> Result<Client, AppError> {
         let config = Config::from_env().unwrap();
-        let host = config.mongo.host;
-        let port = config.mongo.port;
-        let mongo_addr = format!("mongodb://{}:{}/", host, port);
 
-        Client::with_uri_str(&mongo_addr).await.map_err(|err| {
-            let sub_logger = logger.new(o!("cause" => err.to_string()));
-            crit!(sub_logger, "Error connecting to mongo");
-            AppError::Internal(err.to_string())
-        })
+        Client::with_uri_str(&config.mongo.uri)
+            .await
+            .map_err(|err| {
+                let sub_logger = logger.new(o!("cause" => err.to_string()));
+                crit!(sub_logger, "Error connecting to mongo");
+                AppError::Internal(err.to_string())
+            })
     }
 }
